f5396bd8889fe7ca247af0d3744bfe227003ad15
CryptoPascal31 <CryptoPascal31@users.noreply.github.com>
2026-08-26 07:49:04 +0200

    Changes

--- scripts/summarize_signatures.py
+++ scripts/summarize_signatures.py
@@ -2,39 +2,48 @@ from pathlib import Path
 
 import re
 import json
+from operator import itemgetter
 
-pattern = re.compile(r"^sigs-(?P<name>[^-]+)-(?P<domain>[^-]+)$")
-
+VALIDATORS_RE = re.compile(r"^sigs-(?P<name>[^-]+)-(?P<domain>[^-]+)$")
 CHECKPOINT_RE = re.compile(r"^checkpoint_(\d+)_with_id\.json$")
+
+DOMAINS = ["kadena","eth"]
 
 DATA=Path("data")
 
-total = {"kadena":0, "eth":0}
-validators = {"kadena":{}, "eth":{}}
+total = {}
+validators = {}
 
-for x in DATA.iterdir():
+def checkpoints(validator_dir):
+  for cp in validator_dir.iterdir():
+    match = CHECKPOINT_RE.match(cp.name)
+    if match:
+      yield int(match.group(1))
 
-  if x.is_dir():
-    m = pattern.match(x.name)
-    if m:
-      validator = m.group("name")
-      domain = m.group("domain")
+def get_max_checkpoint(validator_dir):
+  return max(checkpoints(validator_dir), default=0)
 
-      height = 0
-      for cp in x.iterdir():
-        match = CHECKPOINT_RE.match(cp.name)
+def get_validators(domain):
+  for d in DATA.iterdir():
+    if d.is_dir():
+      m = VALIDATORS_RE.match(d.name)
+      if m and m.group("domain") == domain:
+        yield (d, m.group("name"))
 
-        if match:
-          checkpoint_id = int(match.group(1))
-          height = max(checkpoint_id, height)
 
-      total[domain] = max(total[domain], height)
+for domain in DOMAINS:
+  validators[domain] = {}
 
-      validators[domain][validator] = {"name": validator, "height":height}
+  for (v_dir, validator) in get_validators(domain):
+    validators[domain][validator] = {"name": validator, "height":get_max_checkpoint(v_dir), "ok":False}
 
-for d in ["kadena", "eth"]:
-  for val in validators[d].values():
-    val["ok"] = total[d] == val["height"]
+  max_domain_cp = max( map(itemgetter("height"), validators[domain].values()))
+
+  for v in validators[domain].values():
+    if v["height"] == max_domain_cp:
+      v["ok"] = True
+
+  total[domain] = max_domain_cp
 
 with open("summary.json","w") as fd:
   json.dump( {"total": total, "validators":validators}, fd, indent=4)
